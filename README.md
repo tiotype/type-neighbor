6ce7ce4429ab5676855ef09ebb9a1634c840e231
Jon Young <jonyoung@Jons-iMac.local>
2021-05-12 18:59:31 +0200

    Update README and change simple index to fonts page

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Type Neighbor
 
-Type Neighbor is a Gatsby/React-based type tester boilerplate. It is meant for type designers or foundries who use React or Gatsby for their specimen sites or portfolios. The initial state of Type Neighbor is built with six fonts from the Google Fonts library as a means to demonstrate how it can work. It currently supports static font families or single fonts. Variable font tools and full glyph sets will be added in the future.
+Type Neighbor is a Gatsby-based static type tester app. It is meant for type designers or foundries who use React or Gatsby for their specimen sites or portfolios. The initial state of Type Neighbor is built with six fonts from the Google Fonts library as a means to demonstrate how it can work. It currently supports static font families or single fonts. Variable font tools and full glyph should be added in the future.
 
 [https://typeneighbor.com](https://typeneighbor.com/)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,48 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { Link } from "gatsby"
 import { Helmet } from "react-helmet"
 
 import { Container, Row, Col } from "react-bootstrap"
-import Layout from "../components/layoutHome"
+import Layout from "../components/layout"
+import fontsData from "../data/fonts.json"
 
-const IndexPage = () => {
+function IndexPage() {
+  const [fonts, setFonts] = useState([])
+
+  useEffect(() => {
+    setFonts(
+      fontsData.fonts.map(item => ({
+        ...item,
+      }))
+    )
+  }, [])
+
   return (
     <>
-      <Helmet bodyAttributes={{ class: "home-section" }} />
+      <Helmet bodyAttributes={{ class: "fonts-section" }} />
       <Layout>
-        <Container fluid className="home home-contain">
-          <Row>
-            <Col>
-              <p className="header">
-                Type Neighbor is a Gatsby/React-based type tester boilerplate.
-                It is meant for type designers or foundries who use React or
-                Gatsby for their specimen sites or portfolios. The initial state
-                of Type Neighbor is built with six fonts from the Google Fonts
-                library as a means to demonstrate how it can work. It currently
-                supports static font families or single fonts. Variable font
-                tools and full glyph sets will be added in the future.
-              </p>
-              <p className="intro">
-                <Link to="/fonts">Try Type Neighbor</Link>
-              </p>
-            </Col>
-          </Row>
+        <Container fluid className="fonts-contain">
+          {fonts.map((item, i) => (
+            <Row key={i}>
+              {/* Big */}
+              <Col className="d-md-block d-none" md={12}>
+                <div>
+                  <p className={item.className}>
+                    <Link to={"/fonts/" + item.route}>{item.fontName}</Link>
+                  </p>
+                </div>
+              </Col>
+              {/* Small */}
+              <Col className="d-md-none" md={12}>
+                <div>
+                  <p className={item.className}>
+                    <Link to={"/fonts/" + item.route}>{item.fontName}</Link>
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          ))}
         </Container>
       </Layout>
     </>
